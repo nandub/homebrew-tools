d104747ef228124186a602bcec36632447675921
Fernando Ortiz <fortiz2k@gmail.com>
2013-09-29 12:54:44 -0500

    add uthash.rb

---
+++ uthash.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Uthash < Formula
+  homepage 'http://troydhanson.github.io/uthash/'
+  head 'https://github.com/troydhanson/uthash.git'
+
+  def install
+    cd src do
+      include.install 'uthash.h'
+    end
+  end
+end
