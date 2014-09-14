3be16b7e3ae0380055db856cb2cb910f1773d6c8
Fernando Ortiz <fortiz2k@gmail.com>
2014-09-13 23:21:57 -0500

    update pkgconf 0.9.6

--- pkgconf.rb
+++ pkgconf.rb
@@ -2,10 +2,14 @@ require 'formula'
 
 class Pkgconf < Formula
   homepage 'https://github.com/pkgconf/pkgconf'
-  url 'http://rabbit.dereferenced.org/~nenolod/distfiles/pkgconf-0.9.3.tar.bz2'
-  sha256 '00033a1080a81dfff7cd7ef98d2b054e4cf627376df92a67b98f2e0c243e7f11'
+  url 'https://github.com/pkgconf/pkgconf/archive/pkgconf-0.9.6.tar.gz'
+  sha256 'aeadf9f35364a78e9a10f800f95e93280f847bd69d36dd69554d7deae72ad0ab'
+
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
 
   def install
+    system "./autogen.sh"
     args = %W[
         --prefix=#{prefix}
       ]
