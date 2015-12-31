857b2ff67fd2882abc3b84d5a8795d678efd39ac
Fernando Ortiz <fortiz2k@gmail.com>
2015-12-31 14:39:25 -0600

    pkgconf 0.9.12

--- pkgconf.rb
+++ pkgconf.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pkgconf < Formula
   homepage 'https://github.com/pkgconf/pkgconf'
-  url 'https://github.com/pkgconf/pkgconf/archive/pkgconf-0.9.6.tar.gz'
-  sha256 'aeadf9f35364a78e9a10f800f95e93280f847bd69d36dd69554d7deae72ad0ab'
+  url "https://github.com/pkgconf/pkgconf/archive/pkgconf-0.9.12.tar.gz"
+  sha256 "d31a19e22908e5bf277454261222da0640115844f7a62672dbe9e92061d547c5"
 
   depends_on "automake" => :build
   depends_on "autoconf" => :build
